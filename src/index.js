4196cd7e20f418cd63a655f510ecd6f3792ad86a
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-01-06 12:44:37 +0300

    FETCH: Dom display

--- src/index.js
+++ src/index.js
@@ -12,4 +12,38 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+
+  //GET all toys and render as cards on the DOM
+
+  function display(item){
+    let location = document.querySelector("#toy-collection")
+    let toy = document.createElement("div")
+    toy.className = "card"
+    location.appendChild(toy)
+
+      // for (let item of data){
+      
+      // toy.innerHTML = `<img src= "${item.image}" >`
+        
+      // }
+    //   console.log(data)
+    
+     
+      toy.innerHTML = `<img src= "${item.image}" >`
+     
+   
+  }
+  
+  
+  fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(data => data.map(function(item){
+      display(item)
+  } ))
+  //Use a form to create a new card and use POST to persist
+  //click like button and count on the DOM.
+
 });
+
+
